94381539c533b2121a7cef3fd314cfa032ce6178
Lucas <lucasrian.dev@gmail.com>
2026-02-27 14:48:27 -0300

    js concluido por hora

--- script.js
+++ script.js
@@ -1,6 +1,59 @@
 const formulario = document.querySelector("#form-bug");
 const input = document.querySelector("#input-titulo");
-const select = document.querySelectorAll("#select-nivel");
+const select = document.querySelector("#select-nivel");
 const listaBugs = document.querySelector("#lista-bugs");
 
 let bugs = JSON.parse(localStorage.getItem("listaDeBugs")) || [];
+
+formulario.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const valor = input.value;
+  if (valor === "") {
+    alert("Descreva o bug!");
+    return;
+  }
+
+  const novoBug = {
+    id: Date.now(),
+    titulo: valor,
+    nivel: select.value,
+  };
+
+  bugs.push(novoBug);
+
+  localStorage.setItem("listaDeBugs", JSON.stringify(bugs));
+
+  renderizar();
+
+  input.value = "";
+});
+
+function renderizar() {
+  listaBugs.innerHTML = "";
+  bugs.forEach((item, index) => {
+    const divCard = document.createElement("div");
+    divCard.classList.add("card-bug");
+
+    divCard.innerHTML = `
+    <div class="info">
+    <h3>${item.titulo}</h3>
+    <span class='tag'> ${item.nivel}${item.nivel}</span>
+    </div>
+    `;
+
+    const btnDelete = document.createElement("button");
+    btnDelete.innerHTML = "❌";
+    btnDelete.classList.add("btn-deletar");
+
+    btnDelete.addEventListener("click", () => {
+      bugs.splice(index, 1);
+      localStorage.setItem("listaDeBugs", JSON.stringify(bugs));
+      renderizar();
+    });
+
+    divCard.appendChild(btnDelete);
+    listaBugs.append(divCard);
+  });
+}
+
+renderizar();
